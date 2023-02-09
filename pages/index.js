9df0d93b19e4e46af6fed7f6dea7435f151dcd2e
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-02-09 15:05:36 -0800

    initial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Inter } from '@next/font/google'
+import Survey from './Survey'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Survey />
+    </>
+  )
+}
